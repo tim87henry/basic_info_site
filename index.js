10f98d7664db0631d82e9f27e345dc4192c839ce
Timothy Henry <tim87henry@gmail.com>
2022-04-27 22:36:08 +1000

    Rewrote the site using Express js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
+
+// Commenting out previous vanilla node js code
+
+/*
 const server = http.createServer((req,res) => {
     const file = (req.url === "/")? "index.html":req.url+".html";
     fs.readFile(path.join(__dirname,"public",file), (err, content) => {
@@ -17,4 +23,24 @@ const server = http.createServer((req,res) => {
 });
     
 server.listen(5000, () => console.log("Server started ...."))
+*/
 
+app.get('/',function(req, res) {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+});
+
+app.get('/about',function(req, res) {
+    res.sendFile(path.join(__dirname,'public/about.html'));
+});
+
+app.get('/contact',function(req, res) {
+    res.sendFile(path.join(__dirname,'public/contact.html'));
+});
+
+app.get('*',function(req, res) {
+    res.sendFile(path.join(__dirname,'public/404.html'));
+});
+
+app.listen(5000, function() {
+    console.log("Start listening on port 5000 ....")
+})
